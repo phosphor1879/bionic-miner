d019b4dfd2b11b66ae3f8f12562e4d0354618faf
Phosphor <coldcathode@protonmail.com>
2019-07-28 02:53:50 -0700

    nvidia

---
+++ 01-ubuntu1804-nvidia-gtx1xxx-setup.sh
@@ -0,0 +1,16 @@
+set -euxo pipefail
+sudo sed -i s/GRUB_TIMEOUT=0/GRUB_TIMEOUT=2/g /etc/default/grub
+sudo update-grub
+
+sudo add-apt-repository ppa:graphics-drivers
+
+
+sudo apt update
+sudo apt upgrade
+
+
+sudo apt install xterm nvidia-390 -y
+
+sudo nvidia-xconfig
+
+echo "you should reboot and make sure nvidia-smi is working and then run part 2"
---
+++ 02-ubuntu1804-nvidia-gtx1xxx-setup.sh
@@ -0,0 +1,41 @@
+set -euxo pipefail
+
+# get x16r miner
+wget https://github.com/trexminer/T-Rex/releases/download/0.12.1/t-rex-0.12.1-linux-cuda9.1.tar.gz
+mkdir t-rex
+tar -zxvf t-rex-0.12.1-linux-cuda9.1.tar.gz -C t-rex
+
+cat <<EOF > start.sh
+#!/bin/bash
+
+while true
+do
+$PWD/t-rex/t-rex -a x16r -o stratum+tcp://stratum.icemining.ca:3648 -u FPMz6eFy4fYn5B4y85GR52LWRjKXbavRde.$HOSTNAME -p c=PHL
+sleep 5
+done
+EOF
+chmod a+x start.sh
+
+cat <<EOF > start-tmux.sh
+#!/bin/bash
+while true
+do
+tmux new-session -A -s miner '$PWD/start.sh'
+sleep 5
+done
+EOF
+chmod a+x start-tmux.sh
+
+cat <<EOF > start-xterm.sh
+#!/bin/bash
+xterm -e $PWD/start-tmux.sh
+EOF
+chmod a+x start-xterm.sh
+
+mkdir -p ~/.config/autostart
+cat <<EOF > ~/.config/autostart/mining.desktop
+[Desktop Entry]
+Name=mining
+Exec=$PWD/start-xterm.sh
+Type=Application
+EOF
